add1dbd79399bdd2525168f5fd5f2e506034d742
mwvane <bzishvili57@gmail.com>
2024-06-07 10:56:11 +0400

    Fix Section bug

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -9,4 +9,10 @@ export class SectionComponent {
   @Input() tittle: string = ''
   @Input() borderBottom: boolean = false
   @Input() subSection: boolean = false
+  getclassName(){
+    if(this.borderBottom && this.subSection) return "border-bottom sub-section"
+    else if(this.borderBottom) return "border-bottom"
+    else if(this.subSection) return "sub-section"
+    return ""
+  }
 }
