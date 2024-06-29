af97270191125e00b6c52811d2d36873438d3465
mwvane <bzishvili57@gmail.com>
2024-06-30 01:57:49 +0400

    Create solid button

---
+++ src/app/Components/solid-button/solid-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Button } from 'primeng/button';
+
+@Component({
+  selector: 'app-solid-button',
+  templateUrl: './solid-button.component.html',
+  styleUrl: './solid-button.component.css'
+})
+export class SolidButtonComponent extends Button {
+  @Input() tittle: string = 'solid btn'
+  @Input() isLoading: boolean = false
+}
